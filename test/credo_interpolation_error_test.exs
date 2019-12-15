f31bca9a6c06219be51bf39af8808c105b122fb0
Victor Rodrigues <victorc.rodrigues@gmail.com>
2019-12-15 17:48:28 +0100

    rm

--- test/credo_interpolation_error_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule CredoInterpolationErrorTest do
-  use ExUnit.Case
-  doctest CredoInterpolationError
-
-  test "greets the world" do
-    assert CredoInterpolationError.hello() == :world
-  end
-end
